dadd24b5d9e0912336369a65367aff55d37dd0fe
Kir4Kun <rkbl4ze@gmail.com>
2022-04-24 23:29:42 +0530

    Fixes

--- api/src/routes/db.ts
+++ api/src/routes/db.ts
@@ -28,7 +28,6 @@ export async function getRouteByBusId(id: string) {
         .from('routes')
         .select(`
             id,
-            bus:busId(*),
             stopOrder,
             busStop:busStopId(*)
         `)
