430f2440ec7ddcbd0ef1983388e0e85da4dd12f9
Kir4Kun <rkbl4ze@gmail.com>
2022-04-24 22:59:41 +0530

    Fixes key

--- api/src/supabase.ts
+++ api/src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://oaqjdqstnjhhvbooxecf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcWpkcXN0bmpoaHZib294ZWNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0ODU3NTA5NSwiZXhwIjoxOTY0MTUxMDk1fQ.lvoLIf3kUWlUzLa4U7oUA6UD6LXDKFHdDdIR4wiDtV4'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcWpkcXN0bmpoaHZib294ZWNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDgwMTcxMSwiZXhwIjoxOTY2Mzc3NzExfQ.PadZuCLriPpTMS78DIZOV0prnr8izXffS1b9E2JmHS8'
 export const supabase = createClient(supabaseUrl, supabaseKey)
