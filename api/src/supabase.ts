ddb5e3fbf7c0722ecb408698266bd5c66fd29f91
Kir4Kun <rkbl4ze@gmail.com>
2022-04-24 23:03:21 +0530

    Fixes dot env

--- api/src/supabase.ts
+++ api/src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://oaqjdqstnjhhvbooxecf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcWpkcXN0bmpoaHZib294ZWNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDgwMTcxMSwiZXhwIjoxOTY2Mzc3NzExfQ.PadZuCLriPpTMS78DIZOV0prnr8izXffS1b9E2JmHS8'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey ?? '')
